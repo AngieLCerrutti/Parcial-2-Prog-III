1fb0e8176d94fa66694d840d63238d6aea94475c
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2020-11-20 00:13:48 -0300

    se cargo repositorio

---
+++ cmake-build-debug/CMakeFiles/parcial_2_prog_III.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parcial_2_prog_III.dir/main.cpp.o"
+  "parcial_2_prog_III.pdb"
+  "parcial_2_prog_III"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parcial_2_prog_III.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/parcial_2_prog_III.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for parcial_2_prog_III.
+# This may be replaced when dependencies are built.
