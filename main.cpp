ea3451f80b0327edf681e1237e8503156161194b
Usuario <usuario@MacBook-Pro-de-Usuario.local>
2020-11-21 17:49:08 -0300

    se realizo lista

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include "Estructuras/Dato.h"
+#include "Estructuras/List.h"
+#include "Estructuras/HashEntry.h"
+
 using namespace std;
 
 int main(int argc, char *argv[]) // para poder ejercutar los argumentos
 {
-  /*  if (argc <= 1 || argc >=5) //verifica que este la cantidad correcta de argumentos de 1 a 5 el nombre del archivo tambien se cuenta
+  if (argc < 3 || argc >=5) //verifica que este la cantidad correcta de argumentos de 1 a 5 el nombre del archivo tambien se cuenta
     {
         cout << "Argumentos ingresados incorrectamente" << endl;
         return 0;
-    }*/
+    }
     char *esArchivoCsv = NULL; //buscar una cadena de texto adentro de otra
     esArchivoCsv = strstr(argv[argc - 1], ".csv");
     if (!esArchivoCsv) {
         cout << "No se especifico el archivo csv" << endl;
         return 0;
     }
-    // Create a text string, which is used to output the text file
+    // Carga el archivo
     string myText;
     string Archivocsv = argv[argc - 1];
     // Read from the text file
@@ -26,10 +29,16 @@ int main(int argc, char *argv[]) // para poder ejercutar los argumentos
         cout << "Error al leer el archivo" << endl;
         return 0;
     }
-    while (getline(MyReadFile, myText)) {
-        // Output the text from the file
-        //cout << myText;
+
+    if (strcmp(argv[1],"-estad")==0){
+        List<Dato>Lista;
+        getline(MyReadFile, myText); //salto la primera linea
+        while (getline(MyReadFile, myText, ',')) {
+            Dato d;
+        }
+
     }
+
 
 // Close the file
     MyReadFile.close();
